49f42721f33082c4c417f81d5701fff86c5b517e
dv-mac <dijavaji@gmail.com>
2026-09-02 19:27:47 -0500

    commit inicial
    Co-Authored-By: djvasquezj <djvasquezj@uce.edu.ec>

---
+++ src/test/java/ec/com/technoloqie/crmbot/api/TecCrmbotWsApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.com.technoloqie.crmbot.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecCrmbotWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
